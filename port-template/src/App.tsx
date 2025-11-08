2feab292926d71b371b34ee085f45feea17c1d93
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2025-11-08 22:43:40 +0530

    Feat: Project setup

---
+++ port-template/src/App.tsx
@@ -0,0 +1,13 @@
+import PortTemplate from './components/PortTemplate'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <PortTemplate />
+    </div>
+  )
+}
+
+export default App
+
